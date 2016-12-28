378a00c4984d3da00b3dbb2ee5a0ee9a566004da
Jay-PC\Jay <joosung21@gmail.com>
2016-12-28 23:55:52 +0900

    Add mysql module and connect

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var express = require('express')
 var app = express()
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '111111',
+  database : 'todolist'
+});
+
+connection.connect();
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+connection.end();
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
